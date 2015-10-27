5528242d318357c5b35ea38164d1d0556c7ce327
Matthew Arbesfeld <arbesfeld@gmail.com>
2015-10-27 00:04:14 -0400

    Add support for new asset system, and update integration tests to RN 0.14.0-stable

--- AppHubIntegration/AppHubIntegrationTests/AppHubIntegrationTests.m
+++ AppHubIntegration/AppHubIntegrationTests/AppHubIntegrationTests.m
@@ -7,6 +7,7 @@
  * of patent rights can be found in the PATENTS file in the same directory.
  */
 
+#import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
 #import <XCTest/XCTest.h>
 #import <AppHub/AppHub.h>
@@ -16,6 +17,7 @@
 #import "RCTRedBox.h"
 #import "RCTRootView.h"
 #import "RCTBridgeModule.h"
+#import "RCTText.h"
 
 #define TIMEOUT_SECONDS 2
 
@@ -92,38 +94,7 @@ static XCTestExpectation *_newBuildExpectation;
 
 -(void) testNewBuildJavaScript {
   [AppHubTestUtils stubGetBuildRouteWithJsonName:@"MockResponses/working-abc.json"];
-  [AppHubTestUtils stubS3RouteWithIpaName:@"MockBuilds/React-0.11/hello-world-2.zip"];
-  
-  XCTestExpectation *testExpection = [self expectationWithDescription:@"Expect new JS code"];
-  
-  [[AppHub buildManager] fetchBuildWithCompletionHandler:^(AHBuild *result, NSError *error) {
-      NSURL *jsCodeLocation = [[AppHub buildManager].currentBuild.bundle URLForResource:@"main"
-                                                                          withExtension:@"jsbundle"];
-      RCTRootView *view = [[RCTRootView alloc] initWithBundleURL:jsCodeLocation
-                                                      moduleName:@"HelloWorld"
-                                               initialProperties:nil
-                                                   launchOptions:nil];
-      dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 2 * NSEC_PER_SEC), dispatch_get_main_queue(), ^{
-        BOOL foundElement = [self findSubviewInView:view matching:^BOOL(UIView *view) {
-          if ([view.accessibilityLabel containsString:@"hot code"] &&
-              [view.accessibilityLabel containsString:@"ABC"]) {
-            return YES;
-          }
-          return NO;
-        }];
-        
-        XCTAssertTrue(foundElement, @"Couldn't find element with text in %d seconds", TIMEOUT_SECONDS);
-        [testExpection fulfill];
-      });
-    
-  }];
-  
-  [self waitForExpectationsWithTimeout:5 handler:nil];
-}
-
--(void) testNewBuildImage {
-  [AppHubTestUtils stubGetBuildRouteWithJsonName:@"MockResponses/working-abc.json"];
-  [AppHubTestUtils stubS3RouteWithIpaName:@"MockBuilds/React-0.11/hello-world-images.zip"];
+  [AppHubTestUtils stubS3RouteWithIpaName:@"MockBuilds/React-0.14/hello-world-asset-image.zip"];
   
   XCTestExpectation *testExpection = [self expectationWithDescription:@"Expect new JS code"];
   
@@ -136,10 +107,15 @@ static XCTestExpectation *_newBuildExpectation;
                                                  launchOptions:nil];
     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 2 * NSEC_PER_SEC), dispatch_get_main_queue(), ^{
       BOOL foundElement = [self findSubviewInView:view matching:^BOOL(UIView *view) {
-        return [view isKindOfClass:[UIImageView class]];
+        if ([view isKindOfClass:[RCTText class]]) {
+          RCTText *textView = (RCTText *)view;
+          return [textView.textStorage.string rangeOfString:@"buildIdentifier:ABC"].location != NSNotFound;
+        } else {
+          return NO;
+        }
       }];
       
-      XCTAssertTrue(foundElement, @"Couldn't find element with text in %d seconds", TIMEOUT_SECONDS);
+      XCTAssertTrue(foundElement, @"Couldn't find element with UIImageView in %d seconds", TIMEOUT_SECONDS);
       [testExpection fulfill];
     });
     
@@ -148,48 +124,38 @@ static XCTestExpectation *_newBuildExpectation;
   [self waitForExpectationsWithTimeout:5 handler:nil];
 }
 
-/*
--(void) testNewBuildImageIsRetainedAfterNewBuild {
-  // Test that keep our images even when we load a new build
-  [AppHubTestUtils stubGetBuildRouteWithJsonName:@"MockResponses/working-def.json"];
-  [AppHubTestUtils stubS3RouteWithIpaName:@"MockBuilds/React-0.8/hello-world-images.zip"];
+-(void) testNewBuildImage {
+  [AppHubTestUtils stubGetBuildRouteWithJsonName:@"MockResponses/working-abc.json"];
+  [AppHubTestUtils stubS3RouteWithIpaName:@"MockBuilds/React-0.14/hello-world-asset-image.zip"];
   
-  [self fetchBuildSync];
+  XCTestExpectation *testExpection = [self expectationWithDescription:@"Expect new JS image"];
   
-  NSURL *jsCodeLocation = [[AppHub buildManager].currentBuild.bundle URLForResource:@"main"
-                                                                      withExtension:@"jsbundle"];
-  
-  [AppHubTestUtils stubGetBuildRouteWithJsonName:@"MockResponses/no-build.json"];
-  
-  [self fetchBuildSync];
-  
-  RCTRootView *view = [[RCTRootView alloc] initWithBundleURL:jsCodeLocation
-                                                  moduleName:@"HelloWorld"
-                                               launchOptions:nil];
-  
-  NSDate *date = [NSDate dateWithTimeIntervalSinceNow:TIMEOUT_SECONDS];
-  BOOL foundElement = NO;
-  NSString *redboxError = nil;
-  
-  while ([date timeIntervalSinceNow] > 0 && !foundElement && !redboxError) {
-    [[NSRunLoop mainRunLoop] runMode:NSDefaultRunLoopMode beforeDate:[NSDate dateWithTimeIntervalSinceNow:0.1]];
-    [[NSRunLoop mainRunLoop] runMode:NSRunLoopCommonModes beforeDate:[NSDate dateWithTimeIntervalSinceNow:0.1]];
+  [[AppHub buildManager] fetchBuildWithCompletionHandler:^(AHBuild *result, NSError *error) {
+    NSURL *jsCodeLocation = [[AppHub buildManager].currentBuild.bundle URLForResource:@"main"
+                                                                        withExtension:@"jsbundle"];
+    RCTRootView *view = [[RCTRootView alloc] initWithBundleURL:jsCodeLocation
+                                                    moduleName:@"HelloWorld"
+                                             initialProperties:nil
+                                                 launchOptions:nil];
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 2 * NSEC_PER_SEC), dispatch_get_main_queue(), ^{
+      __block UIImageView *element;
+      BOOL foundElement = [self findSubviewInView:view matching:^BOOL(UIView *view) {
+        if ( [view isKindOfClass:[UIImageView class]]) {
+          element = (UIImageView *)view;
+          return YES;
+        } else {
+          return NO;
+        }
+      }];
+      
+      XCTAssertTrue(foundElement, @"Couldn't find element with UIImageView in %d seconds", TIMEOUT_SECONDS);
+      XCTAssertTrue(element.image, @"Couldn't find element with image in %d seconds", TIMEOUT_SECONDS);
+      [testExpection fulfill];
+    });
     
-    redboxError = [[RCTRedBox sharedInstance] currentErrorMessage];
-    
-    foundElement = [self findSubviewInView:view matching:^BOOL(UIView *view) {
-      if ([view isKindOfClass:[UIImageView class]]) {
-        UIImageView *imageView = (UIImageView *)view;
-        XCTAssertNotNil(imageView.image);
-        return YES;
-      }
-      return NO;
-    }];
-  }
+  }];
   
-  XCTAssertNil(redboxError, @"RedBox error: %@", redboxError);
-  XCTAssertTrue(foundElement, @"Couldn't find element with text in %d seconds", TIMEOUT_SECONDS);
+  [self waitForExpectationsWithTimeout:5 handler:nil];
 }
-*/
 
 @end
