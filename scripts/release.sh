5f503efda8d69863db323505d094a0fb28df547f
Matthew Arbesfeld <arbesfeld@gmail.com>
2015-12-15 13:59:12 -0500

    Update release script

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
-read -r -p "Did you remember to bump the version (1) in AHConstants.m (2) in the NPM file (3) in the PodSpec? [y/N] " response
+if [ -z "$1" ]; then
+    echo "usage: releash.sh [tag version]"
+    exit 1
+fi
+
+read -r -p "Releasing version $1: Did you remember to bump the version (1) in AHConstants.m (2) in the NPM file (3) in the PodSpec? [y/N] " response
 case $response in
     [yY][eE][sS]|[yY])
       git tag $1
       git push --tags
       npm publish
-      pod push trunk AppHub.podspec
+      pod trunk push AppHub.podspec
         ;;
     *)
 
