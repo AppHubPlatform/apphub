95ad14978ef4cb4ede1f65bd020e9b46ba737ee1
Matthew Arbesfeld <arbesfeld@gmail.com>
2016-02-01 14:35:57 -0500

    Remove integration tests

--- AppHubIntegration/README.md
+++
@@ -1,4 +0,0 @@
-To build a new bundle for RN:
-
-1. Upgrade React Native dependency in `package.json`.
-2. Run './build-bundle.sh'.
--- AppHubIntegration/build-bundle.sh
+++
@@ -1,5 +0,0 @@
-node ../cli build \
-  --entry-file index.updated.js \
-  -o build.zip \
-  --dev true \
-  --plist-file iOS/Info.plist
